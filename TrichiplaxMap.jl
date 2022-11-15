29256258451f065ec81644de51b1e5d21eb88e7a
Mike Paulin <neuralian@gmail.com>
2022-11-16 12:28:32 +1300

    radialIntensitySample OK

--- TrichiplaxMap.jl
+++ TrichiplaxMap.jl
@@ -26,6 +26,10 @@ end
 
 distance(p::Point2f) = sqrt(p[1]^2 + p[2]^2)
 distance(p::Point2f, q::Point2f) = distance(p-q)
+distance(p::Point2f, Q::Vector{Point2f}) = [distance(p,q) for q in Q]
+# TRUE if any points in Q are within Δ of p
+anycloserthan(Δ::Float64, p::Point2f, Q::Vector{Point2f}) = length(findall(distance(p,Q).<Δ))>0
+
 
 function draw(trichoplax::Trichoplax)
     #trichoplax.plot_data_handle = 
@@ -43,12 +47,34 @@ function move(trichoplax::Trichoplax, dx::Point2f)
 end
 
 
+# sample of n points (type Point2f) from radial intensity function
+# intensity(d,r) is expected number of points in a cell of diameter d um at radius r um
+# with minimum distance Δ between samples (default=1 cell diameter, ie max 1 sample per cell) 
+function radialIntensitySample(tr::Trichoplax, n::Int64, d::Float64, intensity::Function, Δ::Float64=-1.0)
+
+    dA = π*(d/2.0)^2  # area element
+    if Δ<0.0 Δ = d end   # default min spacing
+
+    count = 0
+    P = Array{Point2f,1}(undef, n)  # array of n points, initially undefined
+    while count < n
+        p = Point2f((2.0*rand(2).-1.0).*tr.radius)  # uniform random point in area containing the trichoplax
+        if rand()*dA<intensity(distance(p)) # provisional accept based on density
+            if count<1 || !anycloserthan(Δ, p, P[1:count]) 
+                count = count + 1    
+                P[count] = p           # accept
+            end
+        end
+    end
+    return P
+end
 
 
-############################
-
-
-
+function uniformBand(r::Float32)
+    if (r>100.) & (r<200.) return 1.0
+    else return 0.0
+    end
+end
 
 F = Figure(resolution = (worldWide*pxl_per_um,worldWide*pxl_per_um))
 ax = Axis(F[1,1], aspect = 1)
@@ -63,35 +89,7 @@ worldMap = zeros(pixelWide, pixelWide)
 # construct and draw Trichoplax
 trichoplax  = Trichoplax(tr_diameter/2.0, tr_location)
 
-
-
-
-# sample of n points (type Point2f) from density(r) in disc of radius R
-# maxDensity is an upper bound on density in the region (for rejection sampling)
-# density is 
-function radialDensitySample(tr::Trichoplax, n::Int64, radialDensity::Function)
-
-    count = 0
-    P = Array{Point2f,1}(undef, n)  # array of n points, initially undefined
-    while count < n
-        x = (2.0*rand()-1.0)*tr_diameter/2.0  # random x in disc
-        y = (2.0*rand()-1.0)*tr_diameter/2.0  # random y in disc
-        if (rand()<radialDensity(sqrt(x^2+y^2))) 
-            count = count + 1
-            P[count] = Point2f(x,y)
-        end
-    end
-    return P
-end
-
-
-function uniformBand(r::Float64)
-    if (r>100.) & (r<200.) return 1.0
-    else return 0.0
-    end
-end
-
-#P = radialDensitySample(1000, 500., uniformBand)
+P = radialIntensitySample(trichoplax, 1000, 5.0, uniformBand)
 
 #tr_plthandle = draw(trichoplax)
 
